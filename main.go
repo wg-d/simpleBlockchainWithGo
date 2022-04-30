3653c1cd61eccba7f68cfb53e4eed4cf108e9968
wg-d <wonderdream.dev@outlook.com>
2022-04-30 23:49:08 +0900

    create block && main func

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"time"
 )
 
@@ -9,4 +10,21 @@ type Block struct {
 	transactions []string
 	prevHash     []byte
 	Hash         []byte
+}
+
+func main() {
+	genesisTransactions := []string{"Izzy sent Will 50 bitcoin", "Will sent Izzy 30 bitcoin"}
+	genesisBlock := NewBlock(genesisTransactions, []byte{})
+	fmt.Println("--- First Block ---")
+	printBlockInformation(genesisBlock)
+
+	block2Transactions := []string{"John sent Izzy 30 bitcoin"}
+	block2 := NewBlock(block2Transactions, genesisBlock.Hash)
+	fmt.Println("--- Second Block ---")
+	printBlockInformation(block2)
+
+	block3Transactions := []string{"Will sent Izzy 45 bitcoin", "Izzy sent Will 10 bitcoin"}
+	block3 := NewBlock(block3Transactions, block2.Hash)
+	fmt.Println("--- Third Block ---")
+	printBlockInformation(block3)
 }
