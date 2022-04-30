3f33a7adde2af22bc38e6e894cebaba210ae0d3e
wg-d <wonderdream.dev@outlook.com>
2022-04-30 23:49:52 +0900

    add new block func

--- main.go
+++ main.go
@@ -28,3 +28,13 @@ func main() {
 	fmt.Println("--- Third Block ---")
 	printBlockInformation(block3)
 }
+
+func NewBlock(transactions []string, prevHash []byte) *Block {
+	currentTime := time.Now()
+	return &Block{
+		timestamp:    currentTime,
+		transactions: transactions,
+		prevHash:     prevHash,
+		Hash:         NewHash(currentTime, transactions, prevHash),
+	}
+}
